9e1f50bd0a1a6c25436bbc4bb00040127ab7b804
ian <ibowditch@gmail.com>
2021-07-14 16:59:14 +1000

    Removed sqlalchemy_mixins (didn't seem to work) from app.py. Cleaned out old code in app,py.

--- app.py
+++ app.py
@@ -2,53 +2,46 @@ from flask import Flask, render_template, request
 import datetime
 import sqlalchemy as sa
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_mixins import AllFeaturesMixin
 from sqlalchemy import Column, Integer, Text, Float, DateTime
+import dateutil.parser
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 db = SQLAlchemy(app)
 
 ######### Models #########
-class BaseModel(db.Model, AllFeaturesMixin):
-    __abstract__ = True
-    pass
 
-######## Initialize ########
-BaseModel.set_session(db.session)
-
-class Weather(BaseModel):
+class Weather(db.Model):
     id =    sa.Column(Integer, primary_key=True)
     ts =    sa.Column(DateTime, unique=False)
     temp =  sa.Column(Float, unique=False)
     rh =    sa.Column(Float, unique=False)
+
+    def __repr__(self):
+        return 'ts %s' % self.ts
 
 db.create_all()
 
 
 @app.route('/')
 def index():
-    # return 'Hello World!'
     return render_template('index.html')
 
-@app.route('/cakes')
-def cakes():
-    neww = Weather.create(ts=datetime.datetime.now(), temp=10.5, rh=65.0)
-    return 'Yummy cakes!'
-
-@app.route('/hello/<name>')
-def hello(name):
-    r = request
-    return render_template('page.html', name=name)
+# @app.route('/cakes')
+# def cakes():
+#     return 'Yummy cakes!'
+#
+# @app.route('/hello/<name>')
+# def hello(name):
+#     return render_template('page.html', name=name)
 
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     r = request
-    tms = r.values['temp_c']
-    hds = r.values['relative_humidity']
-    tm = float(tms)
-    hd = float(hds)
-    neww = Weather.create(ts=datetime.datetime.now(), temp=tm, rh=hd)
+    neww = Weather(ts=dateutil.parser.parse(r.values['timestamp']), temp=float(r.values['temp_c']), rh=float(r.values['relative_humidity']))
+    db.session.add(neww)
+    db.session.commit()
+    allw = Weather.query.all()
     return 'update'
 
 
