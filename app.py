7f87e8afc275d579e0267c4bbe54a5e801dfad98
ian <ibowditch@gmail.com>
2021-07-15 10:50:10 +1000

    Added bsstationj.html - uses jinja2. Added /latest to app.py to display latest weather using bsstationj.html Added logging to app.py, but couldn't install systemd JournalHandler properly.

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Text, Float, DateTime
 import dateutil.parser
 
+import logging
+logger = logging.getLogger(__name__)
+
+# from systemd import journal       #TODO can't get to install systemd in this project somehow
+import logging.handlers
+# Log all messages to the system journal
+# loghandler = JournalHandler(SYSLOG_IDENTIFIER=SYSLOG_ID)
+# logger = logging.getLogger(SYSLOG_ID)
+# logger.addHandler(loghandler)
+# logger.setLevel(logging.DEBUG)  # TODO change to INFO in production
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 db = SQLAlchemy(app)
@@ -18,7 +29,8 @@ class Weather(db.Model):
     rh =    sa.Column(Float, unique=False)
 
     def __repr__(self):
-        return 'ts %s' % self.ts
+        return f'Weather at {self.ts}: Temp={self.temp} RH={self.rh}'
+
 
 db.create_all()
 
@@ -27,23 +39,20 @@ db.create_all()
 def index():
     return render_template('index.html')
 
-# @app.route('/cakes')
-# def cakes():
-#     return 'Yummy cakes!'
-#
-# @app.route('/hello/<name>')
-# def hello(name):
-#     return render_template('page.html', name=name)
+@app.route('/latest')
+def latest():
+    lastrep = Weather.query.order_by(-Weather.ts).first()           # No last() available, so get latest
+    return render_template('bsstationj.html', rep=lastrep)
 
 @app.route('/update', methods=['GET', 'POST'])
 def update():
-    r = request
-    neww = Weather(ts=dateutil.parser.parse(r.values['timestamp']), temp=float(r.values['temp_c']), rh=float(r.values['relative_humidity']))
+    neww = Weather(ts=dateutil.parser.parse(request.values['timestamp']),
+                   temp=float(request.values['temp_c']),
+                   rh=float(request.values['relative_humidity']))
     db.session.add(neww)
     db.session.commit()
-    allw = Weather.query.all()
+    logger.info(neww)
     return 'update'
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
