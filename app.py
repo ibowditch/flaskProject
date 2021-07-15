173377b2934a0d28ee70e9c924e21a65dd4969ed
ian <ibowditch@gmail.com>
2021-07-15 17:11:50 +1000

    Added new jinja values to bsstationj.html. Added wind readings to Weather model

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import datetime
 import sqlalchemy as sa
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, Text, Float, DateTime
+from sqlalchemy import Column, Integer, Text, String, Float, DateTime, desc
 import dateutil.parser
 
 import logging
@@ -23,22 +23,30 @@ import logging.handlers
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 ######### Models #########
 
 class Weather(db.Model):
-    id =    sa.Column(Integer, primary_key=True)
-    ts =    sa.Column(DateTime, unique=False)
-    temp =  sa.Column(Float, unique=False)
-    rh =    sa.Column(Float, unique=False)
+    id =        sa.Column(Integer,  primary_key=True)
+    ts =        sa.Column(DateTime, unique=False)
+    temp =      sa.Column(Float,    unique=False)
+    rh =        sa.Column(Float,    unique=False)
+    wind_dird = sa.Column(Integer,  unique=False)               # Wind direction in degrees
+    wind_dirc = sa.Column(Text,     unique=False)               # Win direction as compass reading
+    wind_sp =   sa.Column(Float,    unique=False)               # in km/h
 
     def __repr__(self):
-        return f'Weather at {self.ts}: Temp={self.temp} RH={self.rh}'
+        return f'Weather at {self.ts}: Temp={self.temp} RH={self.rh} wind={self.wind_sp} dir={self.wind_dirc}'
 
 
 db.create_all()
 
+def degToCompass(num):
+    val=int((num/22.5)+.5)
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    return arr[(val % 16)]
 
 @app.route('/')
 def index():
@@ -46,17 +54,21 @@ def index():
 
 @app.route('/latest')
 def latest():
-    lastrep = Weather.query.order_by(-Weather.ts).first()           # No last() available, so get latest
+    lastrep = Weather.query.order_by(desc(Weather.ts)).first()           # No last() available, so get latest
     return render_template('bsstationj.html', rep=lastrep)
 
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     neww = Weather(ts=dateutil.parser.parse(request.values['timestamp']),
                    temp=float(request.values['temp_c']),
-                   rh=float(request.values['relative_humidity']))
+                   rh=float(request.values['relative_humidity']),
+                   wind_dird=int(request.values['wind_degrees']),
+                   wind_dirc=degToCompass(int(request.values['wind_degrees'])),
+                   wind_sp=float(request.values['wind_mph'])*1.60934)                   # Convert mph -> kph
     db.session.add(neww)
     db.session.commit()
-    logging.info(neww)
+    # logging.info(neww)
+    print(neww)
     return 'update'
 
 if __name__ == '__main__':
